39738e29795f6189b01af2b3c7c11243529b8cb4
Edward Lee <edwardlee0323@gmail.com>
2016-05-17 13:58:20 +0000

    final update

--- tbay.py
+++ tbay.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, desc
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -31,14 +31,6 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    
-    # Item to Bid relationship
-    item_id = Column(Integer, ForeignKey("items.id"), nullable=False)
-    item = relationship("Item", backref="bid")
-    
-    # Item to User relationship
-    bidder_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    bidder = relationship("User", backref="bids")
 
 class Bid(Base):
     __tablename__ = "bid"
@@ -48,11 +40,11 @@ class Bid(Base):
     
     # Item to Bid relationship
     item_id = Column(Integer, ForeignKey("items.id"), nullable=False)
-    item = relationship("Item", backref="bid")
+    item = relationship("Item", backref="bids")
     
     # Bid to User relationship
     bidder_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    bidder = relationship("User", backref="bid")
+    bidder = relationship("User", backref="bids")
 
 Base.metadata.create_all(engine)
 
@@ -60,22 +52,23 @@ if __name__ == '__main__':
     peter = User(username="pgriffin", password="hyahya")
     chris = User(username="cgriffin", password="cgriffin")
     luis = User(username="lgriffin", password="whatarush")
+    meg = User(username="mgriffin", password="imbeautiful")
     
     baseball = Item(name="KGJ's baseball", 
                     description="Ken Griffey Jr's Signed Baseball",
-                    owner=peter)
+                    owner=meg)
     
     bids = [
         Bid(price=100.0, bidder=chris, item=baseball),
         Bid(price=150.0, bidder=luis, item=baseball),
-        Bid(price=250.0, bidder=chris, item=baseball),
+        Bid(price=250.0, bidder=peter, item=baseball),
         Bid(price=300.0, bidder=luis, item=baseball),
-        Bid(price=500.0, bidder=chris, item=baseball)
+        Bid(price=500.0, bidder=peter, item=baseball)
         ]
     
-    session.add_all([peter, chris, luis, baseball] + bids)
+    session.add_all([peter, chris, luis, meg, baseball] + bids)
     session.commit()
     
-    highest_bid = (session.query(Bid))
+    highest_bid = session.query(Bid).order_by(desc(Bid.price)).first()
     
-    print(highest_bid.bidder.username)
+    print("{} bought it for {}").format(highest_bid.bidder.username, highest_bid.price)
